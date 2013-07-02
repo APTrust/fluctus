13c1ea91f07b3bbe918cd227db43bf1dabf4db16
Andrew Curley <andrew.curley@gmail.com>
2013-07-02 10:49:31 -0400

    Initial commit of Rails 4.0.0 and Hydra-head 6.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fluctus::Application.config.secret_key_base = '932df434628cbe8221e0e9e9054aaebb72a3a69821c9c42929b8717d2e3d968c6f87d1d3ecf8cbec3f542497a04d436cce3ad1209a9b3085dededf3747aa28c2'
