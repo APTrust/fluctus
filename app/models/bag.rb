73933a6fc6eb41f0fde3865eb949f8558fd02af8
Andrew Curley <andrew.curley@gmail.com>
2013-07-10 21:36:34 -0400

    Bag has many CompressedBags.  Validation to ensure that there is only one CompressedBag is commented out pending determination the numericality of this relationship

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -3,6 +3,19 @@ class Bag < ActiveFedora::Base
   has_file_datastream 'bagContent'
 
   belongs_to :description_object, property: :is_part_of
-  has_one :compressed_bag, property: :is_derivation_of  
+  has_many :compressed_bags, property: :is_derivation_of
 
+  # This method is being stashed until we determine the relationship 
+  # between bags and compressed bags.
+  #
+  # validate :validate_compressed_bag_count
+
+  # # A Bag object should only have one associated CompressedBag.  ActiveFedora (as of version 6.4)
+  # # does not have a 'has_one' relationship.  Therefore we are forced to create a validation to enforce
+  # # the singularity of that relationship.
+  # def validate_compressed_bag_count
+  #   if self.compressed_bags.count > 1
+  #     errors.add('A bag cannot have more than 1 associated CompressedBag.')
+  #   end
+  # end
 end
