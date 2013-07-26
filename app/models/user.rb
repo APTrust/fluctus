9ba9ab7bbe60fa5b38c1260eafa51fe49d53d3f6
Andrew Curley <andrew.curley@gmail.com>
2013-07-26 16:09:33 -0400

    Add method documentation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,21 +20,26 @@ class User < ActiveRecord::Base
   validates :institution_name, presence: true
   validates_inclusion_of :institution_name, in: Institution.all.map(&:name)
 
-  # Method added by Blacklight; Blacklight uses #to_s on your
-  # user class to get a user-displayable login/identifier for
-  # the account. 
   # Custom format validations.  See app/validators
   validates :name, person_name_format: true
   validates :email, email: true
   validates :phone_number, phone_format: true
+
+  # Blacklight uses #to_s on youruser class to get a user-displayable 
+  # login/identifier for the account. 
+  #
+  # Method modified from the Blacklight default.
   def to_s
     name.nil? ? email : name
   end
 
+  # Roles are managed through the hydra-role-management gem.
   def is?(role)
     self.roles.pluck(:name).include?(role.to_s)
   end
 
+  # Since an Institution is an ActiveFedora Object, these two objects cannot be related as normal (i.e. belogns_to)
+  # They will be connected through the Institution.name which should be unique.
   def institution
     return Institution.where(name: self.institution_name).first
   end
@@ -45,6 +50,9 @@ class User < ActiveRecord::Base
     false
   end
 
+  # This method comes from the oauth2 documentation on Github.  The only alteration is that when a user is
+  # not found, a new User with no attributes is created.  This is necessary to disable the creation of
+  # unauthorized users.
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data["email"]).first
