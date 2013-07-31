37898ae9dca261be55c7bf3a9012ce6954f27e27
Andrew Curley <andrew.curley@gmail.com>
2013-07-31 19:47:56 -0400

    Prevent destruction of Institution if User belongs to it.

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -10,6 +10,7 @@ class Institution < ActiveFedora::Base
 
   validates :name, presence: true
   validate :name_is_unique
+  validate :check_for_users, on: :delete
 
   private
 
@@ -18,5 +19,10 @@ class Institution < ActiveFedora::Base
   # we must remove self from the array before testing for uniqueness.
   def name_is_unique
     errors.add(:name, "must be unique") unless !Institution.all.reject{|r| r == self}.map(&:name).include?(self.name)
+  end
+
+  def check_for_users
+    count = User.where(institution_name: self.name).count
+    errors.add(:name, "cannot be deleted because #{count} users are associated with this insitution.") unless count == 0
   end
 end
