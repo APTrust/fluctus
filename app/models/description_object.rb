fe7acae4cf807748ca74b71fa3179493f96ca83b
Andrew Curley <andrew.curley@gmail.com>
2013-08-02 08:48:39 -0400

    Add default permissions settings on DescriptionObject as a before_save callback

--- app/models/description_object.rb
+++ app/models/description_object.rb
@@ -1,6 +1,7 @@
 class DescriptionObject < ActiveFedora::Base
 
   include Hydra::ModelMixins::RightsMetadata
+  before_save :set_permissions
 
   has_metadata "rightsMetadata", type: Hydra::Datastream::RightsMetadata
   has_metadata 'descMetadata', type: Datastream::DescriptionObjectMetadata
@@ -9,8 +10,12 @@ class DescriptionObject < ActiveFedora::Base
 
   delegate :title, to: 'descMetadata', unique: true
   delegate :dpn_status, to: 'descMetadata', unique: true
-  # delegate :institution_name, to: 'descMetadata', unique: true
 
   validates :title, :dpn_status, presence: true
 
+  private
+  def set_permissions
+    self.edit_groups = ['admin', 'institutional_admin']
+    self.read_groups = ['institutional_guest']
+  end
 end
