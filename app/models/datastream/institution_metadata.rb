09f0a2cead8c50cc0c6aa1521dcfe3635454d386
Andrew Curley <andrew.curley@gmail.com>
2013-08-08 15:51:46 -0400

    Change InstitutionMetadata to RDF tripples.

--- app/models/datastream/institution_metadata.rb
+++ app/models/datastream/institution_metadata.rb
@@ -1,4 +1,4 @@
-class Datastream::InstitutionMetadata < ActiveFedora::RdfxmlRDFDatastream
+class Datastream::InstitutionMetadata < ActiveFedora::NtriplesRDFDatastream
   map_predicates do |map|
     map.name(in: RDF::DC, to: 'title') do |index|
       index.as :stored_searchable
