7a3b211ae602f6f7a7ebe45d11adfebc09cee831
Streamweaver <Streamweaver@gmail.com>
2013-08-08 16:51:18 -0400

    Fleshed out bagManifestDatastream tests

--- spec/models/datastreams/bag_manifest_datastream_spec.rb
+++ spec/models/datastreams/bag_manifest_datastream_spec.rb
@@ -7,7 +7,7 @@ describe Datastream::BagManifestDatastream do
     @mf = bag.fileManifest
     @mf.title = "uva_uva_lib_1229365"
     @mf.uri = "https://s3.amazonaws.com/aptrust_test_bags/uva_uva_lib_1229365"
-    @fi1 = @mf.files.build(
+    @fi = @mf.files.build(
       format: "text/plain",
       type: "textfile",
       uri: "https://s3.amazonaws.com/aptrust_test_bags/uva_uva_lib_1229365/bagit.txt",
@@ -22,9 +22,25 @@ describe Datastream::BagManifestDatastream do
     )
   end
 
-  it 'should contain top level properties' do
-    @mf.title.should == ["mybagtitle"]
+  it 'should have properties and files' do
+    @mf.title.should == ["uva_uva_lib_1229365"]
     @mf.files.count.should == 1
-    @mf.uri.should == ["http://www.webstuff.com/mybagtitle"]
+    @mf.uri.should == ["https://s3.amazonaws.com/aptrust_test_bags/uva_uva_lib_1229365"]
+  end
+
+  it "should have valid file properties" do
+    @fi.format.should == ["text/plain"]
+    @fi.type.should == ["textfile"]
+    @fi.uri.should == ["https://s3.amazonaws.com/aptrust_test_bags/uva_uva_lib_1229365/bagit.txt"]
+    @fi.size.should == ["3456"]
+    @fi.checksum.count.should == 1
+    @fi.created.should_not be_empty
+    @fi.modified.should_not be_empty
+  end
+
+  it "should have valid checksum properties" do
+    @fi.checksum.first.algorithm.should == ["md5"]
+    @fi.checksum.first.datetime.should_not be_empty
+    @fi.checksum.first.digest.should == ["ada799b7e0f1b7a1dc86d4e99df4b1f4"]
   end
 end
