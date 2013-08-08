5dfd98c94d0091808c055bf57ea1d9bb85215e11
Streamweaver <Streamweaver@gmail.com>
2013-08-08 09:35:36 -0400

    Adding spec tests for premis datastream

--- app/models/datastream/premis_event_datastream.rb
+++ app/models/datastream/premis_event_datastream.rb
@@ -8,7 +8,10 @@ class EventVocabulary < RDF::Vocabulary("http://multimedialab.elis.ugent.be/user
   property :eventOutcomeInformation
   property :eventDateTime
   property :eventDetail
+  property :linkingObject
+  property :linkingAgent
 
+  property :Event
 end
 
 class Datastream::PremisEventDatastream < ActiveFedora::RdfxmlRDFDatastream
@@ -25,8 +28,11 @@ class Event
   map_predicates do |map|
     map.identifier(to: :identifier, in: EventVocabulary)
     map.type(to: :eventType, in: EventVocabulary)
-    map.date_time(to: :eventDataTime, in: EventVocabulary)
+    map.date_time(to: :eventDateTime, in: EventVocabulary)
     map.detail(to: :eventDetail, in: EventVocabulary)
+    map.outcome_detail(to: :eventDetail, in: EventVocabulary)
     map.outcome_information(to: :eventOutcomeInformation, in: EventVocabulary)
+    map.object(to: :linkingObject, in: EventVocabulary)
+    map.agent(to: :linkingAgent, in: EventVocabulary)
   end
 end
--- spec/models/datastreams/premis_event_spec.rb
+++ spec/models/datastreams/premis_event_spec.rb
@@ -1,8 +1,22 @@
-require 'rspec'
+require 'spec_helper'
 
-describe PremisEvent do
+describe Datastream::PremisEvent do
 
-  it 'should create a new object' do
+  before do
+      @bag = Bag.new
+  end
+
+  it 'should create a valid event' do
+    pe = @bag.premisEvents
+    pe.events.build(
+        identifier: "0hc50321-6d7b-3847-89ag-a8b0fhc1f245",
+        type: "fixity generation",
+        date_time: "2010-08-01T09:08:46-01:00",
+        outcome_detail: "",
+        outcome_infromation: "454c167687fc3ce46e48b62533ea70e804287c413683158c58d49f23fcca397d",
+        object: "binary_object_identifier",
+        agent: "Amazon S3 fixity generator"
+    )
 
   end
 end
