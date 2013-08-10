558e571b49e987fb618957a5be16d4b3cc4e4120
Andrew Curley <andrew.curley@gmail.com>
2013-08-10 14:44:57 -0400

    Improve factories by including traits and attribute evaluation in blocks to ensure unique values.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,25 @@
 
 FactoryGirl.define do
   factory :user, class: "User" do
-    name Faker::Name.name
-    email Faker::Internet.email
-    phone_number Faker::PhoneNumber.phone_number
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    phone_number { Faker::PhoneNumber.phone_number }
     institution_name "Fake University"
   
     factory :aptrust_user, class: "User" do
       institution_name "APTrust"
+    end
+
+    trait :admin do
+      roles { [Role.where(name: 'admin').first] }
+    end
+
+    trait :institutional_admin do
+      roles { [Role.where(name: 'institutional_admin').first] }
+    end
+
+    trait :institutional_user do
+      roles { [Role.where(name: 'institutional_user').first] }
     end
   end
 end
