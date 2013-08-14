86dd67c7651beb0e06089bf1cc59221730349a7f
Andrew Curley <andrew.curley@gmail.com>
2013-08-14 12:51:37 -0400

    Improve Bag Factory.

--- spec/factories/bag.rb
+++ spec/factories/bag.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
       i = rand(100000000)
 
       mf = bag.fileManifest
-      mf.title = "uva_uva-lib_#{i}"
+      mf.title = "uva_uva-lib%3A#{i}"
       mf.uri = "https://s3.amazonaws.com/test_bags/uva_uva-lib%3A#{i}"
       (1..10).to_a.sample.times do |index|
         # Get a sample MIME type for a particular File.
@@ -14,7 +14,7 @@ FactoryGirl.define do
 
         bag.fileManifest.files.build
         bag.fileManifest.files[index - 1].format = "#{format}"
-        bag.fileManifest.files[index - 1].uri = "https://s3.amazonaws.com/aptrust_test_bags/uva_uva_lib_#{i}/bagit.txt"
+        bag.fileManifest.files[index - 1].uri = "#{mf.uri}/datastream_#{i}"
         bag.fileManifest.files[index - 1].size = "#{(0..10000).to_a.sample}"
         bag.fileManifest.files[index - 1].created = "#{Time.now}"
         bag.fileManifest.files[index - 1].modified = "#{Time.now}"
