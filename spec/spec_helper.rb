35388cc11c8007638314c6cfd9a93dd15909d0d0
Andrew Curley <andrew.curley@gmail.com>
2013-08-16 16:38:31 -0400

    Add test for OmniauthCallbacksController to bring up to 100%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,12 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
-require 'coveralls'
-Coveralls.wear!
+# require 'coveralls'
+require 'simplecov'
+
+# push test code to remote and produce locally.
+#Coveralls.wear!
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -18,6 +22,43 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 # Enable fake google sign_ins
 OmniAuth.config.test_mode = true
+
+# this hash is pulled directly from https://github.com/zquestz/omniauth-google-oauth2/wiki
+omniauth_hash ={
+    :provider => "google_oauth2",
+    :uid => "123456789",
+    :info => {
+        :name => "John Doe",
+        :email => "john@company_name.com",
+        :first_name => "John",
+        :last_name => "Doe",
+        :image => "https://lh3.googleusercontent.com/url/photo.jpg"
+    },
+    :credentials => {
+        :token => "token",
+        :refresh_token => "another_token",
+        :expires_at => 1354920555,
+        :expires => true
+    },
+    :extra => {
+        :raw_info => {
+            :id => "123456789",
+            :email => "user@domain.example.com",
+            :verified_email => true,
+            :name => "John Doe",
+            :given_name => "John",
+            :family_name => "Doe",
+            :link => "https://plus.google.com/123456789",
+            :picture => "https://lh3.googleusercontent.com/url/photo.jpg",
+            :gender => "male",
+            :birthday => "0000-06-25",
+            :locale => "en",
+            :hd => "company_name.com"
+        }
+    }
+}
+
+OmniAuth.config.add_mock(:google, omniauth_hash)
 
 # Enable capybara to login and logout users
 include Warden::Test::Helpers
