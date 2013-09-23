872db630a74a9200ef1b53bd5c997cb23319b77f
Andrew Curley <andrew.curley@gmail.com>
2013-09-23 10:07:47 -0400

    Cleanup of bag factory.

--- spec/factories/bag.rb
+++ spec/factories/bag.rb
@@ -5,25 +5,31 @@ FactoryGirl.define do
       # Fabricate a random integer for creating a PID
       i = rand(100000000)
 
-      mf = bag.fileManifest
-      mf.title = "uva_uva-lib%3A#{i}"
-      mf.uri = "https://s3.amazonaws.com/test_bags/uva_uva-lib%3A#{i}"
-      (1..10).to_a.sample.times do |index|
+      @fm = bag.fileManifest
+      @fm.title = "uva_uva-lib%3A#{i}"
+      @fm.uri = "https://s3.amazonaws.com/test_bags/uva_uva-lib%3A#{i}"
+      @files_attributes = []
+      (1..10).to_a.sample.times do
+        n = rand(100000000)
         # Get a sample MIME type for a particular File.
         format = ['application/xml', 'application/rdf+xml', 'image/tiff', 'video/mp4'].sample
-
-        bag.fileManifest.files.build
-        bag.fileManifest.files[index - 1].format = "#{format}"
-        bag.fileManifest.files[index - 1].uri = "#{mf.uri.first}/datastream_#{i}"
-        bag.fileManifest.files[index - 1].size = "#{(0..10000).to_a.sample}"
-        bag.fileManifest.files[index - 1].created = "#{Time.now}"
-        bag.fileManifest.files[index - 1].modified = "#{Time.now}"
-        bag.fileManifest.files[index - 1].checksum.build
-        bag.fileManifest.files[index - 1].checksum.first.algorithm = "md5"
-        bag.fileManifest.files[index - 1].checksum.first.datetime = "#{Time.now}"
-        bag.fileManifest.files[index - 1].checksum.first.digest = SecureRandom.hex
+        @files_attributes << {
+          id: "#{@fm.uri.first}/datastream_#{n}",
+          format: "#{format}",
+          uri: "#{@fm.uri.first}/datastream_#{n}",
+          size: "#{(0..10000).to_a.sample}",
+          created: "#{Time.now}",
+          modified: "#{Time.now}",
+          checksum_attributes: [{
+            algorithm: "md5",
+            datetime: "#{Time.now}",
+            digest: SecureRandom.hex
+          }]
+        }
       end
+      @fm.files_attributes = @files_attributes
       bag.save!
+      bag.description_object.save!
     }
   end
 end
