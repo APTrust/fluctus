da1526a273871a840b198b3c3eabb02b843e12b1
Justin Coyne <justin@curationexperts.com>
2013-12-19 12:51:27 -0600

    Upgrading Rails. Removing selenium-webdriver and binding_of_caller

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -1,15 +1,10 @@
 class GenericFilesController < ApplicationController
   load_and_authorize_resource
   before_filter :authenticate_user!
-  before_action :set_generic_file, only: [:show, :edit, :update]
 
   actions :show, :edit, :update
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_generic_file
-    @generic_file = GenericFile.find(params[:id])
-  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def generic_file_params
