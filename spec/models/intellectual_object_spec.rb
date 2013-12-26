e3fb6380a50e3878677f6dbcf5629c4a81f0f945
Justin Coyne <justin@curationexperts.com>
2013-12-26 09:51:53 -0600

    IntellectualObject's spec wasn't making assertions about groups.

--- spec/models/intellectual_object_spec.rb
+++ spec/models/intellectual_object_spec.rb
@@ -6,118 +6,82 @@ include Aptrust::SolrHelper
 
 describe IntellectualObject do
 
-  let(:subject) { FactoryGirl.create(:intellectual_object) }
-  let(:public_subject) { FactoryGirl.create(:public_intellectual_object) }
-  let(:inst_subject) { FactoryGirl.create(:institutional_intellectual_object) }
-  let(:private_subject) { FactoryGirl.create(:private_intellectual_object) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:identifier) }
   it { should validate_presence_of(:institution) }
   it { should validate_presence_of(:rights)}
 
-  it 'should have a descMetadata datastream' do
-    subject.descMetadata.should be_kind_of IntellectualObjectMetadata
+  describe "An instance" do
+    it 'should have a descMetadata datastream' do
+      subject.descMetadata.should be_kind_of IntellectualObjectMetadata
+    end
+
+    it 'should properly set a title' do
+      subject.title = 'War and Peace'
+      subject.title.should == 'War and Peace'
+    end
+
+    it 'should properly set rights' do
+      subject.rights = 'public'
+      subject.rights.should == 'public'
+    end
+
+    it 'must be one of the standard rights' do
+      subject.rights = 'error'
+      subject.should_not be_valid
+    end
+
+    it 'should properly set a description' do
+      exp = Faker::Lorem.paragraph
+      subject.description = exp
+      subject.description.should == [exp]
+    end
+
+    it 'should properly set an identifier' do
+      exp = SecureRandom.uuid
+      subject.identifier = exp
+      subject.identifier.should == [exp]
+    end
   end
 
-  it 'should properly set a title' do
-    subject.title = 'War and Peace'
-    subject.title.should == 'War and Peace'
-  end
+  describe "A saved instance" do
+    subject { FactoryGirl.create(:intellectual_object) }
+    let(:inst_pid) { clean_for_solr(subject.institution.pid) }
+    after { subject.destroy }
 
-  it 'should properly set rights' do
-    subject.rights = 'public'
-    subject.rights.should == 'public'
-  end
+    it 'must check for generic_files before destory' do
+      item = FactoryGirl.create(:generic_file, intellectual_object: subject)
+      subject.destroy.should be_false
+      item.destroy
+    end
 
-  it 'must be one of the standard rights' do
-    subject.rights = 'error'
-    subject.should_not be_valid
-  end
+    describe "with public access" do
+      subject { FactoryGirl.create(:public_intellectual_object) }
+      it 'should properly set groups' do
+        expect(subject.edit_groups).to match_array ['admin', "Admin_At_#{inst_pid}"]
+        expect(subject.read_groups).to match_array ['institutional_user', 'institutional_admin']
+        expect(subject.discover_groups).to eq []
+      end
+    end
 
-  it 'should properly set a description' do
-    exp = Faker::Lorem.paragraph
-    subject.description = exp
-    subject.description.should == [exp]
-  end
+    describe "with institutional access" do
+      subject { FactoryGirl.create(:institutional_intellectual_object) }
+      it 'should properly set groups' do
+        expect(subject.edit_groups).to match_array ['admin', "Admin_At_#{inst_pid}"]
+        expect(subject.read_groups).to eq ["User_At_#{inst_pid}"]
+        expect(subject.discover_groups).to eq []
+      end
+    end
 
-  it 'should properly set an identifier' do
-    exp = SecureRandom.uuid
-    subject.identifier = exp
-    subject.identifier.should == [exp]
-  end
-
-  it 'must check for generic_files before destory' do
-    item = FactoryGirl.create(:generic_file, intellectual_object: subject)
-    subject.destroy.should be_false
-    item.destroy
-  end
-
-  it 'should properly set discover groups if rights are public' do
-    #public_subject.set_permissions
-    (public_subject.discover_groups.include?('admin') ) &&
-        (public_subject.discover_groups.include?('institutional_admin')) &&
-        (public_subject.discover_groups.include?('institutional_user'))
-  end
-
-  it 'should properly set read groups if rights are public' do
-    #public_subject.set_permissions
-    (public_subject.read_groups.include?('admin') ) &&
-        (public_subject.read_groups.include?('institutional_admin')) &&
-        (public_subject.read_groups.include?('institutional_user'))
-  end
-
-  it 'should properly set edit groups if rights are public' do
-    #public_subject.set_permissions
-    inst_pid = clean_for_solr(public_subject.institution.pid)
-    (public_subject.edit_groups.include?('admin') ) &&
-        (public_subject.edit_groups.include?("Admin_At_#{inst_pid}"))
-  end
-
-  it 'should properly set discover groups if rights are institutional' do
-    #inst_subject.set_permissions
-    inst_pid = clean_for_solr(inst_subject.institution.pid)
-    (inst_subject.discover_groups.include?('admin') ) &&
-        (inst_subject.discover_groups.include?("Admin_At_#{inst_pid}")) &&
-        (inst_subject.discover_groups.include?("User_At_#{inst_pid}"))
-  end
-
-  it 'should properly set read groups if rights are institutional' do
-    #inst_subject.set_permissions
-    inst_pid = clean_for_solr(inst_subject.institution.pid)
-    (inst_subject.read_groups.include?('admin') ) &&
-        (inst_subject.read_groups.include?("Admin_At_#{inst_pid}")) &&
-        (inst_subject.read_groups.include?("User_At_#{inst_pid}"))
-  end
-
-  it 'should properly set edit groups if rights are institutional' do
-    #inst_subject.set_permissions
-    inst_pid = clean_for_solr(inst_subject.institution.pid)
-    (inst_subject.edit_groups.include?('admin') ) &&
-        (inst_subject.edit_groups.include?("Admin_At_#{inst_pid}"))
-  end
-
-  it 'should properly set discover groups if rights are private' do
-    #private_subject.set_permissions
-    inst_pid = clean_for_solr(private_subject.institution.pid)
-    (private_subject.discover_groups.include?('admin') ) &&
-        (private_subject.discover_groups.include?("Admin_At_#{inst_pid}")) &&
-        (private_subject.discover_groups.include?("User_At_#{inst_pid}"))
-  end
-
-  it 'should properly set read groups if rights are private' do
-    #private_subject.set_permissions
-    inst_pid = clean_for_solr(private_subject.institution.pid)
-    (private_subject.read_groups.include?('admin') ) &&
-        (private_subject.read_groups.include?("Admin_At_#{inst_pid}")) &&
-        (private_subject.read_groups.include?("User_At_#{inst_pid}"))
-  end
-
-  it 'should properly set edit groups if rights are private' do
-    #private_subject.set_permissions
-    inst_pid = clean_for_solr(private_subject.institution.pid)
-    (private_subject.edit_groups.include?('admin') ) &&
-        (private_subject.edit_groups.include?("Admin_At_#{inst_pid}"))
+    describe "with private access" do
+      subject { FactoryGirl.create(:private_intellectual_object) }
+      it 'should properly set groups' do
+        expect(subject.edit_groups).to match_array ['admin', "Admin_At_#{inst_pid}"]
+        expect(subject.read_groups).to eq []
+        expect(subject.discover_groups).to eq ["User_At_#{inst_pid}"]
+      end
+    end
   end
 
 end
