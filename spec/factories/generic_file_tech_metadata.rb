07efdbb1500c4c9acaec6d12b4e67060dff1e900
Streamweaver <Streamweaver@gmail.com>
2014-01-09 14:35:45 -0500

    Removed single quotes on uri and added sequence to avoid collisions.

--- spec/factories/generic_file_tech_metadata.rb
+++ spec/factories/generic_file_tech_metadata.rb
@@ -1,9 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
+  sequence(:uri) { |n| "#file:///#{intellectual_object.identifier}/data/#{n}filename.xml" }
+
   factory :generic_file_tech_metadata, :class => 'GenericFileMetadata' do
     format { 'application/xml' }
-    uri { 'file:///#{intellectual_object.identifier}/data/filename.xml' }
+    uri
     size { rand(20000..500000000) }
     created { "#{Time.now}" }
     modified { "#{Time.now}" }
@@ -15,4 +18,5 @@ FactoryGirl.define do
        }]
     }
   end
+
 end
