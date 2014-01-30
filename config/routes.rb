b922c92f99bf0ba49290637ee83ac72f28a4017f
Kelly Croswell <kelly.croswell@aptrust.org>
2014-01-30 18:03:37 -0500

    added change password features

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Fluctus::Application.routes.draw do
 
   devise_for :users
 
-  resources :users  #, only: [:edit] do
-  #  collection do
-  #    patch 'update_password'
-  #  end
-  #end
+  resources :users do
+    patch 'update_password', on: :collection
+    get 'edit_password', on: :member
+  end
+
   resources :generic_files, only: [:show, :destroy], path: 'files' do
     resources :events, only: [:create]
   end
