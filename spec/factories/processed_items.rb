b209ab61be5583a39175742c883e302b34948ac8
Kelly Croswell <kelly.croswell@aptrust.org>
2014-06-04 18:06:36 -0400

    processed item creation bug fixed

--- spec/factories/processed_items.rb
+++ spec/factories/processed_items.rb
@@ -4,11 +4,11 @@ FactoryGirl.define do
   factory :processed_item do
     name { Faker::Lorem.word }
     etag { SecureRandom.uuid }
-    bag_date { Faker::Number.number(8) }
+    bag_date { "2014-06-03 15:28:39 UTC" }
     user { Faker::Name.name }
     institution { FactoryGirl.create(:institution).pid }
     bucket { "aptrust.receiving.#{institution}" }
-    date { Faker::Number.number(4) }
+    date { "2014-06-03 15:28:39 UTC" }
     note { Faker::Lorem.sentence }
     action { Faker::Lorem.word }
     stage { Faker::Lorem.word }
