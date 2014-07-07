b45113391654d813d1de761694668c255147c901
Streamweaver <Streamweaver@gmail.com>
2014-07-07 13:26:22 -0400

    Dates from Processed Items factory are dynamically generated.

--- spec/factories/processed_items.rb
+++ spec/factories/processed_items.rb
@@ -2,13 +2,13 @@
 
 FactoryGirl.define do
   factory :processed_item do
-    name { Faker::Lorem.word }
-    etag { SecureRandom.uuid }
-    bag_date { "2014-06-03 15:28:39 UTC" }
+    name { SecureRandom.uuid + ".tar" }
+    etag { SecureRandom.hex }
+    bag_date { Time.now.utc }
     user { Faker::Name.name }
     institution { FactoryGirl.create(:institution).pid }
     bucket { "aptrust.receiving.#{institution}" }
-    date { "2014-06-03 15:28:39 UTC" }
+    date { Time.now.utc }
     note { Faker::Lorem.sentence }
     action { ['Ingest', 'Fixity Check', 'Retrieval', 'Deletion'].sample }
     stage { ['Fetch', 'Unpack', 'Validate', 'Store', 'Record'].sample }
