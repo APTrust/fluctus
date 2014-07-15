415a430592993116dd4de6dafe545b044331eeb2
A. Diamond <diamond@sheprador.com>
2014-07-15 16:50:09 -0400

    No substantive change, but migration is now reversible

--- db/migrate/20140714203114_default_reviewed_status.rb
+++ db/migrate/20140714203114_default_reviewed_status.rb
@@ -1,5 +1,8 @@
 class DefaultReviewedStatus < ActiveRecord::Migration
-  def change
+  def up
     change_column :processed_items, :reviewed, :boolean, :default => false
+  end
+  def down
+    change_column :processed_items, :reviewed, :boolean
   end
 end
