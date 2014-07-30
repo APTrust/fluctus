8fe7b4da712848a209cdf8b8290e32759591d1f3
Kelly Croswell <kelly.croswell@aptrust.org>
2014-07-30 09:00:17 -0400

    fixed dropdown menu issue

--- app/assets/javascripts/fluctus.js
+++ app/assets/javascripts/fluctus.js
@@ -4,5 +4,12 @@ activate_tabs = function() {
   $('ul.nav-tabs + div.tab-content div.tab-pane:first').addClass('active');
 };
 
+var dropdown;
+dropdown = function() {
+    $('.dropdown-toggle').dropdown();
+};
+
 $(document).ready(activate_tabs);
 $(document).on('page:load', activate_tabs);
+$(document).ready(dropdown);
+$(document).on('page:load', dropdown)
