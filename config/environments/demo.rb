131381c1f65186b89da4a3461da3180c6d7f02b2
A. Diamond <diamond@sheprador.com>
2014-09-19 13:54:34 -0400

    Set up SES config for demo server

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -37,10 +37,12 @@ Fluctus::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'test.aptrust.org',
-    enable_starttls_auto: false
+    :address => "email-smtp.us-east-1.amazonaws.com",
+    :authentication => :login,
+    :enable_starttls_auto => true,
+    :port    => 465,
+    :username => ENV['AWS_SES_USER'],
+    :password => ENV['AWS_SES_PASSWORD']
   }
 
 
