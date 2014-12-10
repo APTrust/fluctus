881b629d2a56d08542be3e3685451422617f56da
Kelly Croswell <kelly.croswell@aptrust.org>
2014-12-10 14:55:10 -0500

    removed leftover redis server setup

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -1,5 +1,1 @@
-config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-Resque.redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true)
 
-Resque.inline = Rails.env.test?
-Resque.redis.namespace = "fluctus:#{Rails.env}"
