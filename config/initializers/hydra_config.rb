5c4a991237c31c29d1c8c7eee97ef8a0a1ba4ba5
Kelly Croswell <kelly.croswell@aptrust.org>
2016-02-19 14:23:54 -0500

    fooling with hydra config

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -16,12 +16,13 @@ if Hydra.respond_to?(:configure)
     # You only need to change these values if you've indexed permissions by some means other than the Hydra's built-in tooling.
     # If you change these, you must also update the permissions request handler in your solrconfig.xml to return those values
     indexer = Solrizer::Descriptor.new(:string, :stored, :indexed, :multivalued)
+    #embargo = EmbargoConfig.new({}, prefix: prefix)
     config[:permissions] = {
-      :discover => {:group =>ActiveFedora::SolrService.solr_name("discover_access_group", indexer), :individual=>ActiveFedora::SolrService.solr_name("discover_access_person", indexer)},
-      :read => {:group =>ActiveFedora::SolrService.solr_name("read_access_group", indexer), :individual=>ActiveFedora::SolrService.solr_name("read_access_person", indexer)},
-      :edit => {:group =>ActiveFedora::SolrService.solr_name("edit_access_group", indexer), :individual=>ActiveFedora::SolrService.solr_name("edit_access_person", indexer)},
-      :owner => ActiveFedora::SolrService.solr_name("depositor", indexer),
-      :embargo_release_date => ActiveFedora::SolrService.solr_name("embargo_release_date", Solrizer::Descriptor.new(:date, :stored, :indexed))
+      :discover => {:group =>ActiveFedora::SolrService.solr_name('discover_access_group', indexer), :individual=>ActiveFedora::SolrService.solr_name('discover_access_person', indexer)},
+      :read => {:group =>ActiveFedora::SolrService.solr_name('read_access_group', indexer), :individual=>ActiveFedora::SolrService.solr_name('read_access_person', indexer)},
+      :edit => {:group =>ActiveFedora::SolrService.solr_name('edit_access_group', indexer), :individual=>ActiveFedora::SolrService.solr_name('edit_access_person', indexer)},
+      :owner => ActiveFedora::SolrService.solr_name('depositor', indexer),
+      :embargo_release_date => ActiveFedora::SolrService.solr_name('embargo_release_date', Solrizer::Descriptor.new(:date, :stored, :indexed))
     }
 
   end
